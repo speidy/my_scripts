ca1bf255c147a144b40fcc066615204bc72e29b5
Idan Freiberg <speidy@gmail.com>
2015-06-21 19:10:17 +0300

    changes for ntlm

--- ntlm_hash_generator/ntlm-generator.py
+++ ntlm_hash_generator/ntlm-generator.py
@@ -1,16 +1,28 @@
 import hashlib,binascii
+from itertools import product
 from sys import argv
 
 def gen_ntlm_hash(password):
 	hash = hashlib.new('md4', password.encode('utf-16le')).digest()
 	return  binascii.hexlify(hash)
 	
+
+def bruteforce_cases(plain_pass, ntlm_hash, sub = ""):
+    l = [(c, c.upper()) if not c.isdigit() else (c, ) for c in plain_pass.lower()]
+    l = ["".join(item) for item in product(*l)]
+    for v in l:
+        if gen_ntlm_hash(v) == ntlm_hash:
+           return v 
+    return "Unknown"
+
 def main():
 	if (len(argv) == 1):
 		print "Usage: %s <Clear Text Password>" % argv[0]
 		return False
-	
-	print "NTLM Hash: " + gen_ntlm_hash(argv[1])
+        elif (len(argv) == 2):
+            print "NTLM Hash: " + gen_ntlm_hash(argv[1])
+        elif (len(argv) == 3):
+            print "Correct one is: " + bruteforce_cases(argv[1], argv[2])
 	return True
 	
 if __name__ == "__main__":
